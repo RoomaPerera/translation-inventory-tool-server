9a2633c5b156ef20c11337b06c9f8c340db48a37
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2025-07-11 15:50:12 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 require('./cron');
 
 const connectDB = require('./src/config/db');
-const { port } = require('./src/config/config');
+const {PORT } = require('./src/config/config');
 const app = require('./src/app');
 
 //connect to db
 connectDB().then(() => {
-    const server = app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+    const server = app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
     process.once('SIGUSR2', () => {
         server.close(() => process.kill(process.pid, 'SIGUSR2'));
--- src/app.js
+++ src/app.js
@@ -7,9 +7,12 @@ const userRoutes = require('./routes/userRoutes');
 //express app
 const app = express();
 
+const logger = require('./middleware/logger');
+
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(logger); //  log all requests
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
@@ -18,6 +21,7 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/activitylogs', require('./routes/activityLogRoutes'));
 
 
 module.exports = app;
